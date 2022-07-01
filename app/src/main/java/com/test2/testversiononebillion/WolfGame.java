6412f784acde8f3be4f2187a1da4eaa876043239
Jonathan <95anjn@gmail.com>
2022-07-01 17:04:07 +0200

    test

---
+++ app/src/main/java/com/test2/testversiononebillion/WolfGame.java
@@ -0,0 +1,14 @@
+package com.test2.testversiononebillion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WolfGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wolf_game);
+    }
+}
